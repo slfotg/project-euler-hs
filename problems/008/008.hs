4a8124eb290be21a77c366d835081729ea8884ef
Sam Foster <slfotg@gmail.com>
2017-11-10 23:45:52 -0600

    Problem 8 solution

---
+++ problems/008/008.hs
@@ -0,0 +1,20 @@
+import Data.Char
+import Data.List
+import System.IO
+
+readDigits :: String -> IO [Int]
+readDigits file = do
+    lines <- fmap lines $ readFile file
+    return . map digitToInt $ intercalate [] lines
+
+getMaxProduct :: Int -> [Int] -> Int
+getMaxProduct iterations digits = getMaxProduct' 1 digits digits
+    where
+        getMaxProduct' i digits'@(d:ds) products
+            | i == iterations = maximum products
+            | otherwise       = getMaxProduct' (i + 1) ds $ zipWith (*) digits' products
+
+main :: IO ()
+main = do
+    digits <- readDigits "008.txt"
+    putStrLn . show $ getMaxProduct 13 digits
