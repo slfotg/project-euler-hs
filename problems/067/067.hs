976a319aac1d17f3e6a1da6ece2d9f3318e64d98
Sam Foster <slfotg@gmail.com>
2017-11-11 14:09:08 -0600

    Problem 67 solution

---
+++ problems/067/067.hs
@@ -0,0 +1,18 @@
+
+readNumbers :: String -> IO [[Int]]
+readNumbers file = do
+    numbers <- fmap (map words) . fmap lines $ readFile file
+    return $ map (map read) numbers
+
+getPartialMax :: [Int] -> [Int] -> [Int]
+getPartialMax top@(x:_) (y:ys) = (x + y) : getPartialMax' top ys
+    where
+        getPartialMax' (a:b:cs) (n:ns) = max (n + a) (n + b) : getPartialMax' (b:cs) ns
+        getPartialMax' (a:[]) (n:_)    = [a + n]
+getPartialMax [] ys = ys
+
+getMax :: [[Int]] -> Int
+getMax = maximum . foldl getPartialMax []
+
+main :: IO ()
+main = fmap getMax (readNumbers "067.txt") >>= putStrLn . show 
