237a77b5a90ee5bccfa1f25843f2e90f41fcb118
Sam Foster <slfotg@gmail.com>
2017-11-16 15:34:44 -0600

    Removed commented code

--- problems/076/076.hs
+++ problems/076/076.hs
@@ -3,10 +3,8 @@ import qualified Data.Map.Strict as Map
 type LookupMap = Map.Map (Int, Int) Int
 
 countSums :: Int -> Int
-countSums x = countSums' Map.empty x (x - 1) --sum $ map (countSums' x) [1 .. (x - 1)]
---
--- f(5)
--- f'(5, 4) = 4, 1
+countSums x = countSums' Map.empty x (x - 1)
+
 countSums' :: LookupMap -> Int -> Int -> Int
 countSums' _ 0 _ = 1
 countSums' _ _ 1 = 1
