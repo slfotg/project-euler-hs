2286ae015abc389bcb4d146f1a4c9021ea9dcd8a
Sam Foster <slfotg@gmail.com>
2017-11-18 12:20:17 -0600

    Problem 21 solution

---
+++ problems/021/021.hs
@@ -0,0 +1,27 @@
+import qualified Data.Map.Lazy as Map
+
+properDivisorSums :: Int -> Map.Map Int Int
+properDivisorSums n = foldl updateDivisors initMap [2 .. n]
+    where
+        initMap :: Map.Map Int Int
+        initMap = Map.fromList $ map (\x -> (x, 1 :: Int)) [2 .. n]
+        updateDivisors :: Map.Map Int Int -> Int ->  Map.Map Int Int
+        updateDivisors m x = foldl updateDivisors' m [x + x, x + 2 * x .. n]
+            where
+                updateDivisors' :: Map.Map Int Int -> Int ->  Map.Map Int Int
+                updateDivisors' m' k = Map.update (\a -> Just (a + x)) k m'
+
+amicableFilter :: Map.Map Int Int -> Int -> Int -> Bool
+amicableFilter m k a
+    | k > 10000 = False
+    | a == 1    = False
+    | a == k    = False
+    | otherwise = (m Map.! a) == k
+
+amicables :: [Int]
+amicables = let divisorSums = properDivisorSums 25320 in
+    map fst . Map.toList
+    $ Map.filterWithKey (amicableFilter divisorSums) divisorSums
+
+main :: IO ()
+main = putStrLn . show $ sum amicables
