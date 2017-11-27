bdd79245d7757011fc489f158a789200dce79683
Sam Foster <slfotg@gmail.com>
2017-11-26 21:27:50 -0600

    replaced 'foldr (&&) True' with 'and'

--- problems/052/052.hs
+++ problems/052/052.hs
@@ -1,8 +1,7 @@
 import Data.List (sort)
 
 isPermuted :: Integer -> Bool
-isPermuted n = foldr (&&) True
-             $ map ((== (sort . show) n) . sort . show . (*n)) [2 .. 6]
+isPermuted n = and $ map ((== (sort . show) n) . sort . show . (*n)) [2 .. 6]
 
 main :: IO ()
 main = putStrLn . show $ filter isPermuted [1 ..] !! 0
